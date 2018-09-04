e844d90c579294ccbfd153909da073ecd3a4d792
Alex-1983 <1983-1111@mail.ru>
2018-09-04 19:42:36 +0500

    Введение в Javascript

---
+++ js/script.js
@@ -0,0 +1,101 @@
+var link = document.querySelector(".company__link-write");
+var popup = document.querySelector(".write-us");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".write-us__form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var textarea = popup.querySelector("[name=textarea]");
+var overlay = document.querySelector(".modal-overlay");
+
+var isStorageSupport = true;
+var storageLogin = "";
+var storageEmail = "";
+
+try {
+  storageLogin = localStorage.getItem("login");
+  storageEmail = localStorage.getItem("email");
+
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  overlay.classList.add("overlay-show");
+
+  if (storageLogin) {
+    login.value = storageLogin;
+    textarea.focus();
+
+  } else {
+    login.focus();
+  }
+
+  if (storageEmail) {
+    email.value = storageEmail;
+  }
+
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  overlay.classList.remove("overlay-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value || !textarea.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+      localStorage.etItem("email", email.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+        overlay.classList.remove("overlay-show");
+      }
+    }
+});
+
+var mapLink = document.querySelector(".company__map");
+var mapPopup = document.querySelector(".map-popup");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show-map");
+    overlay.classList.add("overlay-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show-map");
+    overlay.classList.remove("overlay-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show-map")) {
+        mapPopup.classList.remove("modal-show-map");
+        overlay.classList.remove("overlay-show");
+      }
+    }
+});
+
+
+
+
